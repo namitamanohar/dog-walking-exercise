5237ca618292a71a93f321d0925ce79528fdbd62
namitamanohar <manohar.namita@gmail.com>
2020-03-19 14:20:05 -0500

    first commit

---
+++ DogWalking.sql
@@ -0,0 +1,70 @@
+﻿CREATE TABLE Neighborhood (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL, 
+);
+CREATE TABLE [Owner] (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL, 
+    [Address] VARCHAR(55) NOT NULL, 
+    NeighborhoodId Integer NOT NULL, 
+    Phone VARCHAR(55) NOT NULL, 
+    CONSTRAINT FK_Owner_Neighborhood Foreign Key(NeighborhoodId) References Neighborhood(Id)                    
+
+);
+CREATE TABLE Walker (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL, 
+    NeighborhoodId Integer NOT NULL, 
+    CONSTRAINT FK_Walker_Neighborhood Foreign Key(NeighborhoodId) References Neighborhood(Id)
+);
+CREATE TABLE Dog (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL, 
+    OwnerId INTEGER NOT NULL, 
+    Breed VARCHAR(55) NOT NULL, 
+    Notes VARCHAR(255) NOT NULL, 
+    CONSTRAINT FK_Dog_Owner Foreign Key(OwnerId) References [Owner](Id)                    
+);
+CREATE TABLE Walks (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Date] datetime NOT NULL, 
+    Duration Integer NOT NULL, 
+    WalkerId Integer NOT NULL, 
+    DogId Integer NOT NULL, 
+    CONSTRAINT FK_Walks_Dog Foreign Key(DogId) References Dog(Id), 
+    CONSTRAINT FK_Walks_Walker Foreign Key(WalkerId) References Walker(Id), 
+);
+
+
+
+
+
+
+
+
+
+INSERT INTO Neighborhood ([Name]) VALUES ('North Nashville');
+INSERT INTO Neighborhood ([Name]) VALUES ('Germantown');
+
+
+INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Izzy', 1, 'Rat Terrier', 'Loves Walks');
+INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Sterling', 2, 'Golden Retriever', 'Enjoys retrieving balls');
+INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Jabari', 3, 'Rotweiler', 'Has lots of energy and loves sticsk');
+INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Abby', 4, 'Boston Terrier', 'Enjoys tennis balls');
+INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Sandy', 2, 'Terrier', 'Does not like the rain');
+INSERT INTO Dog([Name], OwnerId, Breed, Notes) VALUES ('Mandy', 2, 'Terrier', 'Loves puddles');
+
+
+
+INSERT INTO Owner([Name], Address,NeighborhoodId, Phone) VALUES('Hannah', 'Burns Street', 1, '645372'); 
+INSERT INTO Owner([Name], Address,NeighborhoodId, Phone) VALUES('Steve', 'Kellow Street', 2, '235372'); 
+INSERT INTO Owner([Name], Address,NeighborhoodId, Phone) VALUES('Cindy', 'Ann Street', 1, '825372'); 
+INSERT INTO Owner([Name], Address,NeighborhoodId, Phone) VALUES('Reenie', 'Jefferson Street', 2, '8374372'); 
+
+INSERT INTO Walker([Name], NeighborhoodId) VALUES('Jansen',1); 
+INSERT INTO Walker([Name], NeighborhoodId) VALUES('Kevin',2); 
+
+
+INSERT INTO Walks([Date],Duration,WalkerId,DogId) VALUES('02/12/20', 40, 1, 2)
+INSERT INTO Walks([Date],Duration,WalkerId,DogId) VALUES('04/15/20', 30, 2, 4);
+INSERT INTO Walks([Date],Duration,WalkerId,DogId) VALUES('03/17/20', 45, 1, 1);
---
+++ DogWalkingExercises.sql
@@ -0,0 +1,56 @@
+﻿SELECT o.[name] AS 'Owner Name', n.[name] AS 'Neighborhood Name'
+FROM Owner o
+LEFT JOIN Neighborhood n ON 
+o.NeighborhoodId =  n.Id 
+
+SELECT o.[name] AS 'Owner Name', n.[name] AS 'Neighborhood Name'
+FROM Owner o
+LEFT JOIN Neighborhood n ON 
+o.NeighborhoodId =  n.Id 
+WHERE o.Id = 3 
+
+SELECT w.[Name]
+FROM Walker w
+ORDER BY w.[Name] desc 
+
+SELECT DISTINCT Breed
+FROM Dog 
+
+SELECT d.[name] AS 'Dog Name', o.[name] AS 'Owner Name', n.[name] AS 'Neighborhood Name'
+FROM Dog d 
+LEFT JOIN [Owner] o 
+ON o.Id = d.OwnerId 
+LEFT JOIN Neighborhood n 
+ON n.Id =o.NeighborhoodId 
+
+SELECT o.[name] As 'Owner Name', COUNT(OwnerId) AS 'Dog Count'
+FROM Dog d 
+LEFT JOIN [Owner] o 
+ON o.Id = d.OwnerId
+GROUP BY OwnerId, o.[name]
+
+SELECT COUNT(WalkerId) As 'Walker Count', wa.[Name] AS 'Walker Name'
+FROM Walks w 
+LEFT JOIN Walker wa
+ON w.WalkerId = wa.Id
+GROUP BY WalkerId, wa.[Name]
+
+SELECT COUNT(NeighborhoodId) As 'Walker Count', n.[name] AS 'Neighborhood Name'
+FROM Walker w 
+LEFT JOIN Neighborhood n
+ON w.NeighborhoodId = n.Id
+GROUP BY NeighborhoodId, n.[name]
+
+SELECT d.[name] AS 'Dog Name'
+FROM Dog d 
+LEFT JOIN Walks w 
+ON w.DogId = d.Id
+WHERE w.[date] <'03/19/2020' and w.[date] >'03/12/2020'
+
+SELECT d.[name] AS 'Dog Name'
+FROM Dog d 
+LEFT JOIN Walks w 
+ON w.DogId = d.Id
+GROUP BY d.[name]
+HAVING COUNT(w.Id)=0 
+
